e8ca7a2a78e669ec8646be1e8aa3d8d5e6021c39
keerthanamuruge <keerthuofficial2001@gmail.com>
2022-11-01 20:30:20 +0530

    [income-expenses][login serialize updation]

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -30,8 +30,15 @@ class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, min_length=3)
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
     username = serializers.CharField(max_length=255, min_length=6, read_only=True)
-    tokens = serializers.defaultdict()
+    tokens = serializers.SerializerMethodField()
 
+    def get_tokens(self, obj):
+        user = User.objects.get(email=obj['email'])
+
+        return {
+            "access": user.tokens()['access_token'],
+            "refresh": user.tokens()['refresh_token']
+        }
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'tokens']
